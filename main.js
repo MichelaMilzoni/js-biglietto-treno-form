188be47f5848759de04e6c22d86804d457c6e7ae
MichelaMilzoni <michelamilzoni0@gmail.com>
2025-04-10 17:53:39 +0200

    scritto javascript

--- main.js
+++ main.js
@@ -25,4 +25,78 @@
 // ? MILESTONE 3:
 // ? Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile, 
 // ? raffinando la parte di HTML e CSS in modo da renderla esteticamente gradevole.
+// ? ------------------------------------------------------------------------------
 
+// INIZIO ESERCIZIO 
+
+// * creo una variabile per conservare i dati inseriti dall'utente
+// * e creare una card direttamente da javascript
+const userData = {};
+
+// * FUNZIONE PER BOTTONE CANCELLA DATI
+document.getElementById('button-cancel').addEventListener('click', function(cancel) {
+    document.querySelector('.input-name').value = "";
+    document.querySelector('.input-surname').value = "";
+    document.getElementById('input-km').value = "";
+    document.getElementById('inputAge').value = "Choose...";
+    console.log("Campi di input resettati");
+    });
+
+
+    // * FUNZIONE PER BOTTONE CONFERMA DATI 
+document.getElementById('button-confirms').addEventListener("click", function (confirm) {
+//! document.getElementById: selezionare un elemento HTML in una pagina utilizzando il suo attributo ID
+//! addEventListener:        ascolta un evento 
+//! change:                  si verifica quando il valore di un input, 
+//!                          come un campo di testo o un menu a tendina, viene modificato.
+//! calculatePrice:          nome della funzione che verrà eseguita quando si verifica 
+//!                          l'evento "change".
+// ? ogni volta che viene cambiato il valore del <select>, la funzione calculatePrice viene chiamata
+
+    // - DICHIARO LE VARIABILI "IMPORTANDO" ID E CLASSI DA HTML - 
+    // * VALORI NUMERICI VARIABILI - INSERITI DALL'UTENTE
+    userData.kilometers = parseInt(document.getElementById('input-km').value);
+    userData.age = document.getElementById('inputAge').value;
+    // * VALORI TEXT - INSERITI DALL'UTENTE
+    userData.name = document.querySelector('.input-name').value;
+    userData.surname = document.querySelector('.input-surname').value;
+//! document.querySelector:  cerca e restituisce il primo elemento HTML che corrisponde 
+//!                          al selettore CSS specificato.
+//!                          Se non trova nessun elemento corrispondente, restituisce null
+//! #input-km input:         Cerco un elemento input contenuto all'interno dell'ID input-km
+//! document.getElementById: selezionare un elemento HTML in una pagina utilizzando il suo attributo ID
+
+    // * VALORI NUMERICI COSTANTI - DEFAULT O CALCOLATI DAL PROGRAMMA
+    const pricePerKm = 0.21;
+    const discount20 = 0.80; // ? sconto del 20 (80% di totalPrice 80/100-forma decimale)
+    const discount40 = 0.60; // ? sconto del 40 (60% di totalPrice 60/100-forma decimale)
+    let discountPerc = 0;
+    let discountAmount = 0;
+    let totalPrice = userData.kilometers * pricePerKm;
+
+    // * CICLO IF PER EFFETTUARE IL CALCOLO OGNI VOLTA CHE VIENE MODIFICATO UN INPUT
+    if (userData.age === "minorenne") {
+        discountPerc = 20;
+        discountAmount = totalPrice * (discountPerc / 100);
+        totalPrice -= discountAmount; 
+    } else if (userData.age === "over65") {
+        discountPerc = 40;
+        discountAmount = totalPrice * (discountPerc / 100);
+        totalPrice -= discountAmount; 
+    }
+
+    // - SOVRASCRIVO userData CON I DATI UTENTE PER LA CARD -
+        userData.inputName || "N/D", 
+        userData.inputSurname || "N/D",
+        userData.kilometers || "N/D",
+        userData.ageGroup || "N/D",
+//! uso N/D dopo OR Logico: se valore inputName non è valido 
+//! (null, undefined, "vuoto", 0 o false),
+//! allora uso valore dopo || 
+//! N/D = in questo caso NON DISPONIBILE
+        totalPrice.toFixed(2),
+        discountPerc || 0,
+        discountAmount.toFixed(2),
+
+        console.log("Dati utente salvati:", userData);
+});
